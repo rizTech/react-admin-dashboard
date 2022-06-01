32eef156218ee29d6987efaf08ebb93651823c71
Rizwan <rkhan@nttglobal.net>
2022-06-01 13:36:26 +0530

    ADD: userList

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css'
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar'
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className='container'>
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/userList/UserList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './userList.css'
+
+export default function UserList() {
+  return (
+    <div className='userList'>UserList</div>
+  )
+}
