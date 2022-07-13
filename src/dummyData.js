a32f79c9c2d8d3c6b2a962c48b09b227b00decc0
Rizwan Khan <rmkhan117@gmail.com>
2022-07-13 13:32:42 +0530

    ADD: userlist

--- src/dummyData.js
+++ src/dummyData.js
@@ -72,3 +72,76 @@ export const userData = [
         amt: 2100,
       },
   ];
+
+  export const userRows = [
+    { id: 1, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 2, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 3, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 4, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 5, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 6, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 7, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 8, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 9, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    },
+    { id: 10, 
+      username: 'Jon Snow', 
+      avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg',
+      email: 'jon@gmail.com', 
+      status: 'active', 
+      transaction: '$120.00' 
+    }
+  ];
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,47 +1,70 @@
 import React from 'react'
 import './userList.css'
-import { DataGrid } from '@material-ui/data-grid'
+import { DataGrid } from '@mui/x-data-grid'
+import {MdDeleteOutline} from 'react-icons/md'
+import {userRows} from '../../dummyData'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserList() {
+
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  }
+
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'user', headerName: 'User', width: 200, renderCell: (params) => {
+      return (
+        <div className='userListUser'>
+          <img className='userListImg' src={params.row.avatar} alt="" />
+          {params.row.username}
+        </div>
+      )
+    } },
+    { field: 'email', headerName: 'Email', width: 130 },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
+      field: 'status',
+      headerName: 'Status',
       width: 90,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
+      field: 'transaction',
+      headerName: 'Transaction Volume',
       description: 'This column has a value getter and is not sortable.',
       sortable: false,
       width: 160,
     },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+          <Link to={"/user/id/" + params.row.id}>
+            <button className='userListEdit'>Edit</button>
+          </Link>
+            <MdDeleteOutline className='userListDelete' onClick={() => handleDelete(params.row.id)}/>
+          </>
+        )
+      }
+    }
   ];
 
   return (
     <div className='userList'>
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={8}
+        rowsPerPageOptions={[10]}
         checkboxSelection
+        disableSelectionOnClick
       />
     </div>
   )
